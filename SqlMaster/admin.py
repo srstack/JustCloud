ade23e6423b102a106a13689f338fd598c3c4a32
srstack <39378935+srstack@users.noreply.github.com>
2019-01-22 22:27:03 +0800

    Update admin.py

--- SqlMaster/admin.py
+++ SqlMaster/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from SqlMaster.models import Domain, Users, System, Device, Data, Login, Operation
+
+admin.site.register(Domain)
+admin.site.register(Users)
+admin.site.register(System)
+admin.site.register(Device)
+admin.site.register(Data)
+admin.site.register(Login)
+admin.site.register(Operation)
